5e1d791903b490519a2006ddcf0ac60cdb64146e
Schonne Marc <schonne.marc@orange.fr>
2023-08-26 12:59:04 +0200

    Page CV : mise en place & mise en forme de la grille

--- src/pages/cv.jsx
+++ src/pages/cv.jsx
@@ -4,11 +4,114 @@ import '../css/cv.css';
 function Cv() {
   return (
     <main>
-      <section className="banner">
-        <h1 className="h1TitleCvContainer">
-          <Title content="CURRICULUM VITAE" />
-        </h1>
-      </section>
+      <div className="wrapperCv">
+        <section className="bannerCv">
+          <h1 className="h1TitleCvContainer">
+            <Title content="CURRICULUM VITAE" />
+          </h1>
+        </section>
+        {/* <div id="item-2">&nbsp;</div> */}
+        <section className="leftSection">
+          <h2>MES ÉTUDES</h2>
+          <ul className="studies">
+            <li>Lorem ipsum dolor adipisicing elit</li>
+            <li> sit amet consectetur</li>
+            <li> Unde tempora possimus ut eligendi quisquam</li>
+            <li>reiciendis perferendis necessitatibus</li>
+            <li>quia totam quae</li>
+          </ul>
+          <h2>MES ATOUTS</h2>
+          <ul className="studies">
+            <li>Lorem ipsum dolor adipisicing elit</li>
+            <li> sit amet consectetur</li>
+            <li> Unde tempora possimus ut eligendi quisquam</li>
+            <li>reiciendis perferendis necessitatibus</li>
+            <li>quia totam quae</li>
+          </ul>
+          <h2>LANGUES</h2>
+          <ul className="studies">
+            <li>Lorem ipsum dolor adipisicing elit</li>
+            <li> sit amet consectetur</li>
+            <li> Unde tempora possimus ut eligendi quisquam</li>
+            <li>reiciendis perferendis necessitatibus</li>
+            <li>quia totam quae</li>
+          </ul>
+          <h2>DIVERS</h2>
+          <ul className="studies hobbies">
+            <li>Lorem ipsum dolor adipisicing elit</li>
+            <li> sit amet consectetur</li>
+            <li> Unde tempora possimus ut eligendi quisquam</li>
+            <li>reiciendis perferendis necessitatibus</li>
+            <li>quia totam quae</li>
+          </ul>
+        </section>
+        <section className="rightSection">
+          <h2 id="item-0">COMPETÉNCES METIER</h2>
+          <ul id="item-1">
+            <li>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</li>
+            <li>
+              Earum eveniet, ex est aliquid quam aspernatur qui soluta nihil hic
+              facere laborum eligendi corporis itaque dolore
+            </li>
+            <li>temporibus, velit quasi rerum aperiam?</li>
+            <li>d</li>
+            <li>e</li>
+            <li>f</li>
+          </ul>
+          <h2 id="item-3">PARCOURS PROFESSIONNEL</h2>
+          <div id="item-4">&nbsp;</div>
+          <div id="item-5">&nbsp;</div>
+          <div id="item-6">&nbsp;</div>
+          <div id="item-7">&nbsp;</div>
+          <div id="item-8">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae
+            distinctio aperiam impedit? Quia fuga est, voluptate laboriosam
+            vitae a libero ea adipisci ipsam maiores dolores repellat. Commodi
+            magni illo veritatis. Temporibus aut repellendus aspernatur esse
+            omnis. In molestias ab magni sunt dolorem inventore iste veniam,
+            esse provident adipisci dolorum quas rerum aliquid dignissimos error
+            eligendi est iusto facilis tempore asperiores?
+          </div>
+          <div id="item-9">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, et
+            aliquam laboriosam veniam deleniti laudantium atque iste ea cumque
+            ducimus magni perferendis dignissimos id ipsa, rerum, dolore
+            voluptatibus nam numquam! Accusamus fugiat temporibus dolor
+            reiciendis, perferendis consequuntur veniam doloribus, eveniet
+            doloremque dolorem maiores molestiae ipsa aperiam quo eligendi optio
+            debitis, sed sapiente necessitatibus dicta ullam quidem. Veritatis
+            veniam fugit pariatur. Veritatis nobis dicta commodi eum. Neque
+            quaerat quia veritatis quos optio omnis id, quam sint ullam, debitis
+            molestiae provident necessitatibus. Nulla nobis corrupti, possimus
+            ea mollitia aspernatur sit corporis officia? Rem, quidem ullam!
+            Tempore, cumque, ullam doloremque praesentium itaque excepturi vitae
+            distinctio tenetur eveniet nostrum eligendi velit fugit sunt dicta
+            quis provident, veritatis a quisquam exercitationem necessitatibus
+            animi quae libero. Culpa laboriosam aliquam neque id, officiis
+            temporibus a. Dignissimos minima repellendus delectus nostrum rem
+            sed nobis doloremque necessitatibus labore. Mollitia necessitatibus
+            impedit incidunt quisquam non reprehenderit assumenda voluptates et
+            vero. Assumenda voluptate, labore veritatis quisquam officiis quasi
+            minus consequuntur neque ducimus dicta alias vel quis cum fuga,
+            eligendi dolorum saepe sed. Autem est porro expedita doloribus
+            consectetur asperiores id ea? Earum reiciendis autem cupiditate
+            temporibus dolorem ipsum voluptate accusantium, aut corrupti,
+            molestiae odio illum! Nisi officia eos voluptatum, ratione aperiam
+            quasi. Deserunt tempora animi sint corporis, provident impedit esse
+            praesentium! Officiis aperiam vitae exercitationem, modi alias
+            possimus autem at quam architecto maxime laboriosam ut, illum
+            assumenda hic sequi consequatur ex, ipsam enim. Magnam deserunt
+            beatae ea similique inventore officia nam! Animi provident veritatis
+            magni maiores aperiam voluptates consequatur earum alias nulla.
+            Accusantium ducimus ipsa minus repudiandae, provident aut. Animi
+            magnam cupiditate expedita! Consectetur, impedit voluptatibus
+            dolorum ut quia enim neque. Distinctio officiis ut harum fugit a
+            neque repudiandae incidunt, omnis architecto autem fuga accusamus,
+            quae animi, dolores corrupti molestiae? Fugiat repudiandae nobis
+            totam fuga reprehenderit dolor. Reprehenderit voluptate qui atque!
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
