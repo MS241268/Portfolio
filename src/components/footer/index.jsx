905817aa1950cb9b2eddb0a068963ebb1fd23a4d
Schonne Marc <schonne.marc@orange.fr>
2023-09-11 10:21:13 +0200

    Footer : Ajout du lien Github

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,26 +1,32 @@
 import './index.css';
-import { AiTwotoneMail } from 'react-icons/ai';
+import { AiTwotoneMail, AiOutlineGithub } from 'react-icons/ai';
 import { BsLinkedin } from 'react-icons/bs';
 const linkedIn = `https://www.linkedin.com/in/?${process.env.LINKEDIN}`;
+const gitHub = `https://github.com/?${process.env.GITHUB}`;
 function Footer() {
   const instantYear = new Date().getFullYear();
   return (
     <footer>
       <div className="contactDiv">
-        <a href={`${linkedIn}`}>
-          <span>
-            <BsLinkedin className="iconsFooter"></BsLinkedin>
-            LinkedIn
-          </span>
-        </a>
-
         {/*cryptage adresse mail */}
         <a href="mailto:%73&#99;h&#111;&#110;ne%2e%6d&#97;&#114;&#99;&#64;&#111;%72a&#110;g%65%2e%66r">
           {/*****/}
-          <span>
-            <AiTwotoneMail className="iconsFooter"></AiTwotoneMail>
-            Me Contacter
-          </span>
+          <i className="iconsFooter">
+            <AiTwotoneMail className="iconsLink"></AiTwotoneMail>
+          </i>
+          <span> Me Contacter</span>
+        </a>
+        <a href={`${linkedIn}`}>
+          <i className="iconsFooter">
+            <BsLinkedin className="iconsLink"></BsLinkedin>
+          </i>
+          <span>LinkedIn</span>
+        </a>
+        <a href={`${gitHub}`}>
+          <i className="iconsFooter">
+            <AiOutlineGithub className="iconsLink"></AiOutlineGithub>
+          </i>
+          <span>Github</span>
         </a>
       </div>
       <p>© {instantYear} Marc Schonne. All rights reserved</p>
